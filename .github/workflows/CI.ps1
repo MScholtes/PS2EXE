c1ac04f772d1673f19e30d20c544f76c5c6a61f3
steve02081504 <steve02081504@EDEN.fukwold>
2023-12-10 22:36:03 +0800

    CI Draft

---
+++ .github/workflows/CI.ps1
@@ -0,0 +1,19 @@
+try{
+	. $PSScriptRoot/../../Module/ps2exe.ps1
+	& $PSScriptRoot/../../Examples/BuildExamples.ps1
+}catch{}
+
+if($error){
+	$error | ForEach-Object {
+		Write-Output "::error file=$($_.InvocationInfo.ScriptName),line=$($_.InvocationInfo.ScriptLineNumber),col=$($_.InvocationInfo.OffsetInLine),endColumn=$($_.InvocationInfo.OffsetInLine),tittle=error::script error"
+		Write-Output "::group::script stack trace"
+		Write-Output $_.ScriptStackTrace
+		Write-Output "::endgroup::"
+		Write-Output "::group::error details"
+		Write-Output $_
+		Write-Output "::endgroup::"
+	}
+	exit 1
+}
+
+Write-Output "Nice CI!"
